eedd5d35b4889551bd2f1c1675080ce8f0e95cb9
Dariell Vasquez <dariell@Dariells-Air.fios-router.home>
2017-11-14 00:29:19 -0500

    Uploaded short clips of app working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,27 +11,31 @@ var connection = mysql.createConnection({
 //  Customer Module
 var customer = {
   customerSearch: function () {
-    connection.query("Select product_name FROM products", function (err, res) {
+    connection.query("SELECT * FROM products", function (err, res) {
+      if (err) throw err;
       console.log("Products available: ");
-
+      // Log all results of the SELECT statement
       var productArr = [];
-
       for (var i = 0; i < res.length; i++) {
-        productArr.push(res[i].product_name);
+        // pushing each id to an array so that I can validate the user's response. This way, if their product id isn't in the array, they have to try again.
+        productArr.push(res[i].item_id)
+        console.log("\nID: " + res[i].item_id +
+          "\nProduct: " + res[i].product_name +
+          "\nDepartment: " + res[i].department_name +
+          "\nPrice: " + res[i].stock_quantity + "\n-------------");
       }
-      productArr.forEach(function (product) {
-        console.log(product);
-      });
       inquirer.prompt([{
-        name: "name",
+        name: "id",
         type: "input",
-        message: "What is the name of the item you would like to purchase?",
+        message: "What is the id of the item you would like to purchase?",
         validate: function (product) {
-          if (productArr.indexOf(product) < 0) {
-            console.log("\nPlease make sure you used the correct name!");
-            return false;
+          for (var i = 0; i < productArr.length; i++) {
+            if (product == productArr[i]) {
+              return true;
+            }
           }
-          return true;
+          console.log("\nPlease make sure you used the correct id!");
+          return false;
         }
       }, {
         name: "quantity",
@@ -46,7 +50,7 @@ var customer = {
       }]).then(function (val) {
         var query = "SELECT product_name, stock_quantity, price FROM products WHERE ?";
         connection.query(query, {
-          product_name: val.name
+          item_id: val.id
         }, function (err, res) {
           if (res[0].stock_quantity - val.quantity < 0) {
             console.log("Insufficient quantity! \n --------------\nThere are only " + res[0].stock_quantity + " of those left.");
